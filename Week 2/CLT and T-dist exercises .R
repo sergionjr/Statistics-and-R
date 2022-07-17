24e2a3b4938598ef1e83a52ae25a8f235640b5c3
sergionjr <seno7509@colorado.edu>
2022-07-16 21:05:02 -0600

    t-test, t-stat, degrees of freedom

--- Week 2/CLT and T-dist exercises .R
+++ Week 2/CLT and T-dist exercises .R
@@ -1,4 +1,5 @@
 library(downloader)
+library(dplyr)
 url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleMiceWeights.csv"
 filename <- "femaleMiceWeights.csv"
 if(!file.exists("femaleMiceWeights.csv")) download(url,destfile=filename)
@@ -11,6 +12,9 @@ n = 100 #sample size
 sides = 6
 
 p = 1/sides
+
+p=0.5
+n=30
 zs = replicate(10000, {
   x = sample(1:sides, n, replace=TRUE)
   (mean(x == 6) - p) / sqrt(p * (1-p)/n)
@@ -19,3 +23,15 @@ zs = replicate(10000, {
 qqnorm(zs)
 abline(0,1)
 mean(abs(zs) > 2)
+
+
+X <- filter(dat, Diet=="chow") %>% select(Bodyweight) %>% unlist
+Y <- filter(dat, Diet=="hf") %>% select(Bodyweight) %>% unlist
+
+mean(X)
+sd(X)
+
+2 * (1 - pnorm(2/sd(X) * sqrt(12)))
+
+## Standard Error (XBAR - YBAR) = standard deviation of x^ 2 / sample size + ...
+((sd(X)^2)/12 + (sd(Y)^2)/12) %>% sqrt()
